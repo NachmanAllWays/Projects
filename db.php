2fb57bd6e66f371792a2c68a80ae753fa766c741
Nachman Rosen <nachmanrosen@gmail.com>
2019-03-13 21:51:30 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,15 @@
 <?php
-$cs = "mysql:host=localhost;dbname=suppers";
-    $user = "seforim";
-    $password = '1234';
+//$cs = "mysql:host=localhost;dbname=suppers";
+    //$user = "seforim";
+    //$password = '1234';
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$cs = "mysql:host=.$server;dbname=.$db;
+
     
     try {
         $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
