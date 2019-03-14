c48084a6f2519f869b03f091f2a0ca1d6b3ff50b
Nachman Rosen <nachmanrosen@gmail.com>
2019-03-13 21:53:49 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
-//$cs = "mysql:host=localhost;dbname=suppers";
-    //$user = "seforim";
-    //$password = '1234';
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cs = "mysql:host=localhost;dbname=suppers";
+    $user = "seforim";
+    $password = '1234';
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-$cs = "mysql:host=.$server;dbname=.$db;
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"], 1);
+// $cs = "mysql:host=.$server;dbname=.$db;
 
     
     try {
