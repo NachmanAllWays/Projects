2541daea9b2e928bc724d53ce9682fb721359e64
Nachman Rosen <nachmanrosen@gmail.com>
2019-03-25 10:20:42 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,9 +3,12 @@ $id="";
 session_start();
 $id=$_SESSION['id'];
 $name="";
-$cs = "mysql:host=localhost;dbname=suppers";
-    $user = "seforim";
-    $password = '1234';
+// $cs = "mysql:host=localhost;dbname=suppers";
+//     $user = "seforim";
+//     $password = '1234';
+$cs="mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_772f7bcedc89412";
+$user="bed585af83e3c6";
+$password = "dccce2d4";
     
     if(isset($_POST['name'])){
         $name=$_POST['name'];
