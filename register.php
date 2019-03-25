bfe0e1bedf468ddf4ee9ea6416959b36758e76e6
Nachman Rosen <nachmanrosen@gmail.com>
2019-03-25 10:22:06 -0400

    Update register.php

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
     <title>form</title>
  
  
- <link href="/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet"/>
+ <link href="css/bootstrap.min.css" rel="stylesheet"/>
  <style>
    body{
      background-color:lightblue;
